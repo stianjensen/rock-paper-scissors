4ca6d57ccfb0778d2e5de5b978c1282936df2d0d
Stian Jensen <me@stianj.com>
2017-06-28 19:49:51 +0200

    Add basic server

--- server.js
+++ server.js
@@ -1,2 +1,40 @@
 const express = require('express');
 const socket = require('./socket');
+
+const app = express();
+app.use(express.static('client/build'));
+
+const users = {};
+
+const history = [{}];
+
+const sock = socket(conn => {
+  users[conn.id] = {};
+
+  conn.on('data', data => {
+    const message = JSON.parse(data);
+
+    if (message.event === 'register') {
+      users[conn.id] = message.data;
+    }
+
+    if (message.event === 'move') {
+      const lastRound = history[history.length - 1];
+      if (lastRound[conn.id] != null) {
+        lastRound.push({[conn.id]: message.data});
+      } else {
+        lastRound[conn.id] = message.data;
+      }
+      if (lastRound.length === users.length) {
+        sock.broadcast('round', lastRound);
+      }
+    }
+  });
+});
+
+const server = require('http').createServer(app);
+sock.server.installHandlers(server, {prefix: '/socket'});
+
+
+server.listen(7000);
+console.log('Listening on port 7000');
