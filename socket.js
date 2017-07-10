9cbca5087f58435b7b6168c5ace158bfa784e8f5
Stian Jensen <me@stianj.com>
2017-07-10 17:26:02 +0200

    Skip broadcasting to disconnected users

--- socket.js
+++ socket.js
@@ -14,6 +14,9 @@ function socket(onConnectionCallback) {
   function targetedBroadcast(audience, event, data) {
     console.log("audience: " + audience);
     for (const id of audience) {
+      if (!(id in connections)) {
+        continue;
+      }
       console.log("sending to: " + id);
       console.log("from list: " + JSON.stringify(audience));
       connections[id].send(event, data);
